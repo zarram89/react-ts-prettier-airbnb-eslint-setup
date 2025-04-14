2d8341eaef123fa88f5eb8f08cbf984c18dd9003
Ramil <zarram89@gmail.com>
2025-04-14 10:57:39 +0500

    Отрабатывает создание ветки и пул-реквест

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import TestComponent from "./components/TestComponent";
+import TestComponent2 from "./components/TestComponent2";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -30,6 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <TestComponent title="Hello, Test" />
+      <TestComponent2 title="Hello, Test2" />
     </>
   );
 }
---
+++ src/components/TestComponent2.tsx
@@ -0,0 +1,21 @@
+// src/components/TestComponent.tsx
+import { useState } from "react";
+
+interface Props {
+  title: string;
+}
+
+const TestComponent2 = ({ title }: Props) => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        Count: {count}
+      </button>
+    </div>
+  );
+};
+
+export default TestComponent2;
